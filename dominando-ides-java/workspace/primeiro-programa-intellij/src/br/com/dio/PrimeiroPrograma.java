43cc8024cf156261a1ae5aa74702e6cc77ffcfac
RicardoMoraesG <ricardomoraesg@gmail.com>
2022-02-28 20:42:59 -0300

    First commit intellij

--- dominando-ides-java/workspace/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ dominando-ides-java/workspace/primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -2,13 +2,16 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
         System.out.println(gato);
-        System.out.println(gato);
-        System.out.println(gato);
+        Livro livro = new Livro("O problema dos tres corpos", "300");
+        System.out.println(livro);
+
 
        /* int a = 5;
         int b = 3;
@@ -17,4 +20,51 @@ public class PrimeiroPrograma {
 
     }
 
+}
+class Livro {
+
+    private String nome;
+    private String numPaginas;
+
+    public Livro(String nome, String numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas='" + numPaginas + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(String numPaginas) {
+        this.numPaginas = numPaginas;
+    }
 }
