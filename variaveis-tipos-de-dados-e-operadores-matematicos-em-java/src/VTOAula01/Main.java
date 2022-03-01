0ed61d97ff783574b005aedae533b6ccb74b51e6
RicardoMoraesG <ricardomoraesg@gmail.com>
2022-03-01 17:27:49 -0300

    Boas práticas

---
+++ variaveis-tipos-de-dados-e-operadores-matematicos-em-java/src/VTOAula01/Main.java
@@ -0,0 +1,49 @@
+package VTOAula01;
+
+public class Main {
+
+//int i;
+//int i;Não é recomendado como boa prática.
+//int I;
+//int 1a;
+//int _1a;//Não é recomendado como boa prática.
+//int $aq;//Não é recomendado como boa prática.
+
+//i = 5;
+//I = 20;
+//$aq = 7;//Não é recomendado como boa prática.
+
+//final int j = 10;//CONSTANTE.
+//j = 15;//CONSTANTE NÃO ALTERA APÓS A DEFINIÇÃO DO VALOR.
+//int asrn24678md;
+//int asrn246 78md;//erro pois tem espaço na variavel.
+//int asrn2$46778_md = 10;//é válido porem não recomendável.
+//int asrn2$46%78_md;//erro pois contêm caracter especial.
+
+//asrn24678md = 100;
+//asrn24678_md = 10;
+
+//int QuantidadeProduto = 50;
+//int quantidadeProduto;
+//final int NUMERO_TENTATIVAS =5;
+//final int numeroTentativas = 6;
+//int QUANTIDADE_OPCOES =25;
+//int qtdProd;
+
+//System.out.println(i);
+//System.out.println(I);
+//System.out.println(_la);
+//System.out.println($aq);
+//System.out.println($j);
+//System.out.println($asrn24678md);
+//System.out.println($asrn24678_md);
+//System.out.println($asrn24678_4md);
+//
+//System.out.println(quantidadeProduto);
+//System.out.println(NUMERO_TENTATIVAS);
+//System.out.println(QUANTIDADE_OPCOES);
+
+
+
+
+}
