f5c6f3423aa8238e1dcd5c3953e4b10052a7d806
RicardoMoraesG <ricardomoraesg@gmail.com>
2022-03-01 17:55:21 -0300

    Boas práticas

---
+++ variaveis-tipos-de-dados-e-operadores-matematicos-em-java/src/VTOAula02/Main.java
@@ -0,0 +1,59 @@
+package VTOAula02;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    byte b1 = 10;
+    byte b2 = 20;
+
+    //short s1 = 200300;
+    //short s2 = 40000;
+
+    //int i1 = 10000000000;
+    int i2 = 20500;
+
+    long l1 = 1000000004;
+    long l2 = 204004000;
+
+    //float f1 = 4.5;
+    float f2 = 10.68f;
+
+    double d1 = 85.69;
+    double d2 = 99.84d;
+
+    char c1 = 'W';
+    //char c2 = 'Tw';
+    char c3 = '\u0057';
+
+    String st1 = "Fulano";
+    String st2 = "Cicrano";
+    String st3 = "ag dgd se se st KNNFMJF";
+
+//    String dt1 = "09/02/1983";
+
+    boolean boo1 = true;
+    boolean boo2 = false;
+
+
+
+        System.out.println(b1);
+        System.out.println(b2);
+       // System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(b1);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(boo1);
+        System.out.println(boo2);
+
+
+}
+}
