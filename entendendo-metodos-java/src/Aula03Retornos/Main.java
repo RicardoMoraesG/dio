a9cbe586860899fcb92e19dd0ad7de9bfc4750a6
RicardoMoraesG <ricardomoraesg@gmail.com>
2022-03-02 18:56:31 -0300

    Retornos - funcionamento.

---
+++ entendendo-metodos-java/src/Aula03Retornos/Main.java
@@ -0,0 +1,19 @@
+package Aula03Retornos;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Retornos.
+        System.out.println("Exercício retornos.");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5, 3);
+        System.out.println("Área do retangulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+    }
+}
