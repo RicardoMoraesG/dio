329a1c3a17f06d61461fd32923a103ddc395b3e9
RicardoMoraesG <ricardomoraesg@gmail.com>
2022-03-03 21:59:14 -0300

    Orientacao a Objeto

---
+++ OrientacaoObjetosJava/src/Interface/OperacaoMatematica.java
@@ -0,0 +1,13 @@
+package Interface;
+
+interface OperacaoMatematica {
+
+    //define métodos.
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+}
